2fe4aff11cd511df3d9da244ea0de15c1c5e4f73
SamuelFlet <samuelrhfletcher@hotmail.com>
2022-04-28 11:23:01 -0400

    Moved to Linux

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "../styles/App.css";
 import { useQuery, gql } from "@apollo/client";
-import Login from '../components/login';
+import Login from "../components/login";
+import { AUTH_TOKEN } from "../constants";
 
 const HELLO = gql`
-  query getAllPosts{
-  getAllPosts{
-    title
-    author{
-      name
+  query getAllPosts {
+    getAllPosts {
+      title
+      author {
+        name
+      }
     }
   }
-}
 `;
 
 function App() {
@@ -19,17 +20,24 @@ function App() {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-
-  return (
-    <div className="App">
-      <Login/>
-      {data.getAllPosts.map(dog => (
-        <div key={dog.title}>
-          {dog.title}: {dog.author.name}
-        </div>
-      ))}
-    </div>
-  );
+  if (localStorage.getItem(AUTH_TOKEN)) {
+    return (
+      <div className="App">
+        <Login />
+        {data.getAllPosts.map((dog) => (
+          <div key={dog.title}>
+            {dog.title}: {dog.author.name}
+          </div>
+        ))}
+      </div>
+    );
+  }else{
+    return(
+      <div>
+        poop
+      </div>
+    )
+  }
 }
 
 export default App;
